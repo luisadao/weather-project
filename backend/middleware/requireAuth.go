3e398cf3b5dce671629bd35f17335576aae877ed
Luís Adão <luis.santos.adao@gmail.com>
2024-08-22 11:50:38 +0100

    Deleted documentations code

--- backend/middleware/requireAuth.go
+++ backend/middleware/requireAuth.go
@@ -24,12 +24,10 @@ func RequireAuth(c *gin.Context) {
 
 	//Decode/validate it
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// Don't forget to validate the alg is what you expect:
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 
-		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
 		return []byte(os.Getenv("SECRET")), nil
 	})
 	if err != nil {
@@ -38,8 +36,6 @@ func RequireAuth(c *gin.Context) {
 	}
 
 	if claims, ok := token.Claims.(jwt.MapClaims); ok {
-		fmt.Println(claims["foo"], claims["nbf"])
-
 		//check the exp
 		if float64(time.Now().Unix()) > claims["exp"].(float64) {
 			c.AbortWithStatus(http.StatusUnauthorized)
